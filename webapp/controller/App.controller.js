904ed32d6ca605105a782375a6475357e1b3db2d
Anisha Parida <anisha.parida98@gmail.com>
2023-07-11 13:59:40 +0000

    i18n Spanish Translation

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -9,7 +9,32 @@ sap.ui.define([
 
         return Controller.extend("mybankdetails.controller.App", {
             onInit: function () {
+                var appLang;
+                if (navigator.language == "es")
+                    appLang = "i18n_es";
+                else if (navigator.language == "en")
+                    appLang = "i18n";
+                else
+                    appLang = "i18n";
+                var i18nModel = this.getOwnerComponent().getModel(appLang)
+                this.getOwnerComponent().setModel(i18nModel, "i18n")
+            },
 
+            onOpenBankDetails: function () {
+                //create dialog
+                if (!this.moreBankDetails) {
+                    this.moreBankDetails = this.loadFragment(
+                        { name: "mybankdetails.view.fragments.MoreDetails" }
+                    );
+                }
+                this.moreBankDetails.then(
+                    function (oDialog) {
+                        oDialog.open();
+                    });
+            },
+
+            onCloseBankDetails: function () {
+                this.byId("moreBankDetails").close();
             }
         });
     });
